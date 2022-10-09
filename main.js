1f0c21314e9eff3076851b23c1430ba29d07ed82
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-09 11:57:01 -0400

    update

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
-    // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
@@ -18,7 +16,6 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 nav
